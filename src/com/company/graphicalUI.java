b88581184206b9bd889457db17f7829d4d69544d
Mango <mangofighter@proton.me>
2023-06-10 19:00:03 -0600

    began implementing ActionListener, needed to properly use javax.swing

--- src/com/company/graphicalUI.java
+++ src/com/company/graphicalUI.java
@@ -1,12 +1,20 @@
 package com.company;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
-public class graphicalUI {
+public class graphicalUI extends Frame implements ActionListener{
     userInterface theUI = new userInterface();
+    JFrame f;
+    JButton b01;
+    JButton b02;
+    JButton b03;
+    JButton b04;
+    JButton b05;
+    public void actionPerformed (ActionEvent e) {}
     public graphicalUI() {
         //create the window
-        JFrame f = new JFrame("Lorem Ipsum");
+
         //set size of the window (width,height)
         f.setSize(600,400);
         //set behavior when closed
@@ -19,16 +27,16 @@ public class graphicalUI {
         final JTextField tf = new JTextField();
         tf.setBounds(250,75,100,50);
         //make a button
-        JButton b = new JButton("Dolor Sit Amet");
-        b.setBounds(250, 175, 100, 50);
-        b.addActionListener(new ActionListener() {
+        b01 = new JButton("Dolor Sit Amet");
+        b01.setBounds(250, 175, 100, 50);
+        b01.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
                 theUI.addDice();
             }
 
         });
         //add a button
-        f.add(b);
+        f.add(b01);
         f.add(tf);
     }
 }
